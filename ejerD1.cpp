3c6b3cce203a0a66c6cfdbadf9e2396c1d5337c2
Keiryn21 <00061225@uca.edu.sv>
2025-06-04 13:56:23 -0600

    100

---
+++ ejerD1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    //pedir 2 numeros al usuario y verificar cual de ellos es mas cercano al 100//
+
+    int num1=0,num2=0;
+
+    cout<<"por favor ingresa el primer numero"<<endl;
+    cin>>num1;
+
+    cout<<"por favor ingresa el segundo numero"<<endl;
+    cin>>num2;
+
+    if(numero1<numero2)
+    cout<<"el numero"<<numero2;<<"es el mas cercano a 100"<<endl;
+
+    else 
+     (num1&&num2>=100)
+    cout<<"los valores ingresados son incrrectos"<<endl;
+
+
+    return 0 ;
+}
